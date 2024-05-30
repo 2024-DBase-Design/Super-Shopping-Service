6e7006e1983ced790994b1116164dffcf0e0b796
Jasmine Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2024-05-30 19:16:58 -0400

    login almost done

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -4,27 +4,29 @@ import { shrikhand } from "../fonts";
 
 export default function Page() {
   return (
-    <div className="mainContainer">
+    <div className="main-container">
       <h1 className={shrikhand.className}>Silly Stuffs</h1>
-      <div>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <img
+        className="mx-auto h-11 w-auto logo"
+        src="/logo.png"
+        alt="Your Company"
+      />
+      <div className="form-container">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-10 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
+            <h2
+              className={`${shrikhand.className} text-center text-4xl font-bold leading-9 tracking-tight`}
+            >
+              Login
             </h2>
           </div>
 
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
             <form className="space-y-6" action="#" method="POST">
               <div>
                 <label
                   htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block text-sm font-medium leading-6"
                 >
                   Email address
                 </label>
@@ -44,18 +46,10 @@ export default function Page() {
                 <div className="flex items-center justify-between">
                   <label
                     htmlFor="password"
-                    className="block text-sm font-medium leading-6 text-gray-900"
+                    className="block text-sm font-medium leading-6"
                   >
                     Password
                   </label>
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-semibold text-indigo-600 hover:text-indigo-500"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
                 </div>
                 <div className="mt-2">
                   <input
@@ -72,20 +66,22 @@ export default function Page() {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Sign in
+                  Login
                 </button>
               </div>
             </form>
 
-            <p className="mt-10 text-center text-sm text-gray-500">
-              Not a member?{" "}
-              <a
-                href="#"
-                className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              >
-                Start a 14 day free trial
+            <p className="mt-10 text-center text-sm text-white">
+              Don't have an account?{" "}
+              <a href="#" className="font-semibold leading-6">
+                Create a new account
+              </a>
+            </p>
+            <p className="mt-10 text-center text-sm text-white">
+              <a href="#" className="font-semibold leading-6">
+                Create a new account
               </a>
             </p>
           </div>
