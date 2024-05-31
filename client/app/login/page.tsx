f9ed072249ae6078b4f5753d5880a86946db295a
Jasmine-Mogadam <47947909+Jasmine-Mogadam@users.noreply.github.com>
2024-05-31 17:21:34 -0400

    work on login page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import '../../styles/login.scss';
+import { LogoComponent } from '@/components/logo';
+import '@/styles/login.scss';
 import { shrikhand } from '../fonts';
 
 const LoginPage = () => {
   return (
     <div className="main-container">
       <h1 className={shrikhand.className}>Silly Stuffs</h1>
-      <img className="mx-auto h-11 w-auto logo" src="/logo.png" alt="Your Company" />
+      <LogoComponent className="mx-auto h-11 w-auto logo"/>
       <div className="form-container">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-6 lg:px-8">
           <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -56,20 +57,20 @@ const LoginPage = () => {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex mt-10 w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Login
                 </button>
               </div>
             </form>
 
-            <p className="mt-10 text-center text-sm text-white">
+            <p className="mt-5 text-center text-sm text-white">
               Don&apos;t have an account?{' '}
               <a href="#" className="font-semibold leading-6">
                 Create a new account
               </a>
             </p>
-            <p className="mt-10 text-center text-sm text-white">
+            <p className="mt-2 text-center text-sm text-white">
               <a href="#" className="font-semibold leading-6">
                 Create a new account
               </a>
