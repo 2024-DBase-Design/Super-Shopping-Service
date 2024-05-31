3667656c1e030796cdfa630d101f26321e265b4e
Randy Hucker <rhucker@physna.com>
2024-05-31 12:15:40 -0400

    Update stockRoutes.ts

--- src/routes/stockRoutes.ts
+++ src/routes/stockRoutes.ts
@@ -6,8 +6,8 @@ const router = Router();
 router.post('/', stockController.addStock);
 router.get('/', stockController.getStock);
 router.get('/:stockId', stockController.getStockDetails);
-router.get('/product/:stockId', stockController.getStockByProductId);
-router.get('/warehouse/:stockId', stockController.getStockByWarehouseId);
+router.get('/product/:productId', stockController.getStockByProductId);
+router.get('/warehouse/:warehouseId', stockController.getStockByWarehouseId);
 router.put('/:stockId', stockController.updateStock);
 router.delete('/:stockId', stockController.deleteStock);
 
