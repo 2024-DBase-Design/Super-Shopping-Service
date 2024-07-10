1a710c36ad26c72e8b7e8129897a2addd2d515db
Ethan Chaplin <ethan.p.chaplin@gmail.com>
2024-07-10 16:37:35 -0400

    finish bottom nav bar functionality

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,6 +6,7 @@ import '@/styles/noSession.scss';
 import styles from './home.module.scss';
 import { shrikhand } from '../fonts';
 import { ValidationRuleEnum } from '@/components/input/validationRules';
+import NavFooter from '@/components/nav/navFooter';
 import Link from 'next/link';
 import { FormValues } from '@/helpers/formValues';
 import FormComponent, { FormInput, InputTypeEnum } from '@/components/form/form';
@@ -33,18 +34,40 @@ const HomePage = () => {
     <div className={'main-container ' + styles.mainContainer}>
       <div className={styles.topSection}>
         <h1 className={shrikhand.className + ' ' + styles.header}>Silly Stuffs</h1>
-        <input placeholder='Search' className={styles.searchBar} type="text" />
+        <input placeholder="Search" className={styles.searchBar} type="text" />
       </div>
       <div className={styles.homeCard}>
         <div className={styles.homeSection}>
           <h2>Category</h2>
+          <div className={styles.categoryContainer}>
+            <div className={styles.categoryOptions} />
+            <div className={styles.categoryOptions} />
+            <div className={styles.categoryOptions} />
+            <div className={styles.categoryOptions} />
+            <div className={styles.categoryOptions} />
+            <div className={styles.categoryOptions} />
+            <div className={styles.categoryOptions} />
+            <div className={styles.categoryOptions} />
+          </div>
         </div>
         <div className={styles.homeSection}>
           <h2>Recommended</h2>
+          <div className={styles.reccomendedContainer}>
+            <div className={styles.recommendedOptions} />
+            <div className={styles.recommendedOptions} />
+            <div className={styles.recommendedOptions} />
+            <div className={styles.recommendedOptions} />
+            <div className={styles.recommendedOptions} />
+            <div className={styles.recommendedOptions} />
+          </div>
         </div>
       </div>
       <div className={styles.navBar}>
-
+        <NavFooter
+          index={0}
+          icons_Src={['/home.svg', '/cart.svg', '/account.svg']}
+          btn_Functions={[() => console.log('1'), () => console.log('2'), () => console.log('3')]}
+        />
       </div>
     </div>
   );
