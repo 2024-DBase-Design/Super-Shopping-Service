839bd1925bb6625f3f0446d0adb1ff2eb85aaba6
Ethan Chaplin <ethan.p.chaplin@gmail.com>
2024-07-24 16:37:52 -0400

    create product preview component

---
+++ src/components/products/productPreview.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+'use client';
+
+import React from 'react';
+import styles from './productPreview.module.scss';
+import { Product } from '@prisma/client';
+type ProductPreviewProps = {
+  name: string;
+  price: number;
+  image: string;
+  stock: number;
+};
+
+const ProductPreviewComponent: React.FC<ProductPreviewProps> = ({
+    name = "",
+    price = 0,
+    image = "",
+    stock = 0
+}) => {
+  return (
+    <div className={`${styles.product_preview}`}>
+        <img src={image} />
+        <p>{name}</p>
+        <div className={`${styles.price_stock}`}>
+            <p className={`${styles.price}`}>${price}</p>
+            <p className={`${styles.stock}`}>{stock} in stock</p>
+        </div>
+    </div>
+  );
+};
+
+export default ProductPreviewComponent;
