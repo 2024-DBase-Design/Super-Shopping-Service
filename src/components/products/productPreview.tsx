82b783c3eed9859194f6b46c249ef611887e8b8b
Sam Graler <samgraler@gmail.com>
2024-08-01 11:56:47 -0400

    connected and builds

--- src/components/products/productPreview.tsx
+++ src/components/products/productPreview.tsx
@@ -12,21 +12,21 @@ type ProductPreviewProps = {
 };
 
 const ProductPreviewComponent: React.FC<ProductPreviewProps> = ({
-    name = "",
-    price = 0,
-    image = "",
-    stock = 0
+  name = '',
+  price = 0,
+  image = '',
+  stock = 0
 }) => {
   return (
-    <a href=''>
-        <div className={`${styles.product_preview}`}>
-            <img src={image} />
-            <p>{name}</p>
-            <div className={`${styles.price_stock}`}>
-                <p className={`${styles.price}`}>${price}</p>
-                <p className={`${styles.stock}`}>{stock} in stock</p>
-            </div>
+    <a href="">
+      <div className={`${styles.product_preview}`}>
+        <img src={image} />
+        <p>{name}</p>
+        <div className={`${styles.price_stock}`}>
+          <p className={`${styles.price}`}>${price}</p>
+          <p className={`${styles.stock}`}>{stock} in stock</p>
         </div>
+      </div>
     </a>
   );
 };
